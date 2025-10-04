044fb0295798f426ca080985e298ee16351eefee
Your Name <your-email@example.com>
2025-10-04 11:38:17 +0530

    your change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 👇 Add the base property here
+  base: '/AI--Chatbot/', 
   plugins: [react()],
 })
