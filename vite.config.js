3e059d2e085c674f25b55fc9f761622412aeadd7
dead.jpg <jashithnpoojary@gmail.com>
2025-11-25 15:27:06 +0530

    fix netlify base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // 👇 Add the base property here
-  base: '/AI--Chatbot/', 
   plugins: [react()],
+  base: "./"   // <-- correct for Netlify / any hosting
 })
